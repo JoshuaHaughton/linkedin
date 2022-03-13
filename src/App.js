b47f481dbbcd1b709b3c247bee758fe0d624a8c3
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-03-12 21:59:19 -0500

    Create top section of sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import './index.css'
 import Header from './components/Header/Header';
-
+import Sidebar from './components/Sidebar/Sidebar';
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
+      <div className='app__body'>
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import classes from "./Sidebar.module.css";
+import { Avatar } from "@mui/material";
+
+const Sidebar = () => {
+  return (
+    <div className={classes.sidebar}>
+
+      <div className={classes.sidebarTop}>
+        <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80" alt="" />
+        <Avatar className={classes.avatar}/>
+        <h2>Joshua Haughton</h2>
+        <h4>itsjoshuahaughton@gmail.com</h4>
+      </div>
+
+      <div className={classes.stats}>
+        <div className={classes.stat}>
+          <p>Who viewed you</p>
+          <p className={classes.statNumber}>1,743</p>
+        </div>
+        <div className={classes.stat}>
+          <p>Views on post</p>
+          <p className={classes.statNumber}>2,437</p>
+        </div>
+      </div>
+
+      <div className={classes.sidebarBottom}>
+        <p>Recent</p>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Sidebar;
